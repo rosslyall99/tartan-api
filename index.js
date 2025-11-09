2467a2578a0515f627c4c76aa22781df068b9e80
Ross <ross@lyall.me.uk>
2025-11-09 08:47:28 +0000

    Enable CORS for frontend access

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+app.use(cors());
 const mysql = require('mysql2');
 const tartanRoutes = require('./routes/tartans');
 
